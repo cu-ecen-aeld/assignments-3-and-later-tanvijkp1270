0d393463d6645b6fc06470ae27212d0d005703f6
tanvijkp1270 <tanvi.jkp1270@gmail.com>
2025-08-29 03:58:57 +0530

    Save my progress

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -2,26 +2,21 @@
 # Assignment 1 script finder.sh
 # Author: Tanvi sharma
 
-filesdir = $1
-searchstr = $2
-NUMFILES = 0
-MATCHINGLINES = 0
+filesdir=$1
+searchstr=$2
 
 if [ $# -lt 2 ]
 then
 	echo "Not enough arguments"
 	exit 1
-elif [! -d $filesdir]
+elif [ ! -d $filesdir ]
 then 
 	echo "Directory not found, check and enter correct path"
 	exit 1
 else
-	echo "Running filesearch on given directory"
-	
-	MATCHINGLINES = grep -rc $searchstr $filesdir
-	NUMFILES = ls $filesdir | wc -l
-	
-	echo "The number of files are ${NUMFILES} and the number of matching lines are ${MATCHINGLINES}"
+	NUMFILES=$(ls $filesdir | wc -l)
+	MATCHINGLINES=$(grep -rc "$searchstr" "$filesdir" | wc -l)
+	echo "The number of files are $NUMFILES and the number of matching lines are $MATCHINGLINES"
 	exit 0
 fi
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -2,22 +2,26 @@
 # Assignment 1 script writer.sh
 # Author: Tanvi sharma
 
-writefile = $1
-writestr = $2
+writefile=$1
+writestr=$2
+directoryname=$(dirname "$writefile")
 
 if [ $# -lt 2 ]
 then
 	echo "Not enough arguments"
 	exit 1
 else
-	cat > $writefile
+	mkdir -p $directoryname
+	echo "$writestr" > $writefile
 	
-	if [! $? eq 0]
+	ls -l $writefile
+	
+	if [ $? -ne 0 ]
 	then
 		echo "Error creating file"
+		exit 1
 	else
-		cat >> $writestr
-		echo "File created successfully with data ${writestr}"
+		echo "File created successfully with data $writestr"
 		exit 0
 	fi
 fi
