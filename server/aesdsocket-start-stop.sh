6bf59768acbcaeee79ed943e8c20805997d3659f
tanvijkp1270 <tanvi.jkp1270@gmail.com>
2026-01-11 15:25:30 +0530

    Add -d argument after --

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+set -x
+DAEMON=/usr/bin/aesdsocket
+PIDFILE=/var/run/aesdsocket.pid
+DAEMON_OPTS="-d"
+
 case "$1" in
   start)
     echo "Starting aesdsocket"
-     start-stop-daemon -S -n aesdsocket -d -a /usr/bin/aesdsocket
+    start-stop-daemon -S -x "$DAEMON" -p "$PIDFILE" -- "$DAEMON" -d
     ;;
   stop)
     echo "Stopping aesdsocket"
-    start-stop-daemon -K --signal SIGTERM -n aesdsocket -d
+    start-stop-daemon -K -s TERM -p "$PIDFILE"
     ;;
   *)
     echo "Usage: $0 {start|stop}"
